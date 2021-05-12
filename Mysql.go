05a64634b537aa82c0187796b93e1f694af836aa
PassFoam <passingfoam@gmail.com>
2021-05-12 11:42:16 +0800

    添加pkg，增加socks传输超时

---
+++ Mysql.go
@@ -0,0 +1,164 @@
+package Core
+
+import "C"
+import (
+	"Zombie/src/Database"
+	"Zombie/src/Utils"
+	"database/sql"
+	"fmt"
+	_ "github.com/go-sql-driver/mysql"
+	"os"
+)
+
+type MysqlService struct {
+	Username string
+	Password string
+	Utils.IpInfo
+	Utils.MysqlInf
+	Input  string
+	SqlCon *sql.DB
+}
+
+func MysqlQuery(SqlCon *sql.DB, Query string) (err error, Qresult []map[string]string, Columns []string) {
+
+	err = SqlCon.Ping()
+	if err == nil {
+		rows, err := SqlCon.Query(Query)
+		if err == nil {
+			Qresult, Columns = Database.DoRowsMapper(rows)
+
+		} else {
+			fmt.Println("please check your query.")
+			return err, Qresult, Columns
+		}
+	} else {
+		fmt.Println("connect failed,please check your input.")
+		return err, Qresult, Columns
+	}
+
+	return err, Qresult, Columns
+}
+
+func MysqlConnect(User string, Password string, info Utils.IpInfo) (err error, result bool, db *sql.DB) {
+	dataSourceName := fmt.Sprintf("%v:%v@tcp(%v:%v)/?timeout=%ds&readTimeout=%ds&writeTimeout=%ds&charset=utf8", User,
+		Password, info.Ip, info.Port, Utils.Timeout, Utils.Timeout, Utils.Timeout)
+	db, err = sql.Open("mysql", dataSourceName)
+
+	if err != nil {
+		result = false
+		return err, result, nil
+	}
+
+	db.SetMaxOpenConns(60)
+	db.SetMaxIdleConns(60)
+
+	err = db.Ping()
+
+	if err == nil {
+		result = true
+	}
+
+	return err, result, db
+}
+
+func MysqlConnectTest(User string, Password string, info Utils.IpInfo) (err error, result Utils.BruteRes) {
+	err, res, db := MysqlConnect(User, Password, info)
+
+	if err == nil {
+		result.Result = res
+	}
+
+	_ = db.Close()
+
+	return err, result
+}
+
+func (s *MysqlService) Connect() bool {
+	err, _, db := MysqlConnect(s.Username, s.Password, s.IpInfo)
+	if err == nil {
+		s.SqlCon = db
+		return true
+	}
+	return false
+}
+
+func (s *MysqlService) GetInfo() bool {
+	defer s.SqlCon.Close()
+
+	res := GetMysqlBaseInfo(s.SqlCon)
+
+	res.Count = GetMysqlSummary(s.SqlCon)
+
+	fmt.Printf("IP: %v\tServer: %v\nVersion: %v\tOS: %v\nSummary: %v", s.Ip, "Mysql", res.Version, res.OS, res.Count)
+	GetMysqlVulnableInfo(s.SqlCon)
+	GetMysqlGeneralLog(s.SqlCon)
+
+	return true
+}
+
+func (s *MysqlService) Query() bool {
+	err, Qresult, Columns := MysqlQuery(s.SqlCon, s.Input)
+
+	if err != nil {
+		fmt.Println("something wrong")
+		os.Exit(0)
+	} else {
+		Utils.OutPutQuery(Qresult, Columns, true)
+	}
+	return true
+}
+
+func (s *MysqlService) SetQuery(query string) {
+	s.Input = query
+}
+
+func GetMysqlBaseInfo(SqlCon *sql.DB) Utils.MysqlInf {
+	err, Qresult, Columns := MysqlQuery(SqlCon, "select VERSION(),@@version_compile_os")
+	if err != nil {
+		fmt.Println("something wrong at get version")
+	}
+	res := Utils.GetBaseInfo(Qresult, Columns)
+
+	return res
+}
+
+func GetMysqlSummary(SqlCon *sql.DB) string {
+	err, Qresult, Columns := MysqlQuery(SqlCon, "select sum(table_rows) from  information_schema.tables where table_rows is not null")
+
+	if err == nil {
+		Count := Utils.GetSummary(Qresult, Columns)
+		return Count
+	}
+	return ""
+}
+
+func GetMysqlGeneralLog(SqlCon *sql.DB) {
+	err, Qresult, Columns := MysqlQuery(SqlCon, "show VARIABLES like 'general%'")
+	if err != nil {
+		fmt.Println("something wrong in get general log")
+	} else {
+		Utils.OutPutQuery(Qresult, Columns, false)
+	}
+
+}
+
+func GetMysqlVulnableInfo(SqlCon *sql.DB) {
+	err, Qresult, Columns := MysqlQuery(SqlCon, "SHOW VARIABLES LIKE \"secure_file_priv\"")
+	if err != nil {
+		fmt.Println("\nsomething wrong in get secure_file_priv")
+	} else {
+		if len(Qresult) == 1 && len(Columns) == 2 {
+			fmt.Print("\n" + Qresult[0][Columns[0]] + ":\t" + Qresult[0][Columns[1]])
+		}
+	}
+
+	err, Qresult, Columns = MysqlQuery(SqlCon, "show variables like '%plugin%'")
+	if err != nil {
+		fmt.Println("\nsomething wrong in get plugin dir")
+	} else {
+		if len(Qresult) == 1 && len(Columns) == 2 {
+			fmt.Print("\n" + Qresult[0][Columns[0]] + ":\t" + Qresult[0][Columns[1]])
+		}
+	}
+
+}
