52719c69f128181571a190b258cb174c8303e02d
M09ic <m09ician@gmail.com>
2022-06-23 15:41:44 +0800

    refactor!!!

---
+++ v1/pkg/utils/IPHandle.go
@@ -0,0 +1,43 @@
+package utils
+
+import (
+	"math"
+	"net"
+	"strconv"
+	"strings"
+)
+
+func GetIpRange(target string) (start uint, fin uint) {
+	mask := strings.Split(target, "/")[1]
+
+	before, after := getMaskRange(mask)
+
+	ipint := ip2int(target)
+
+	start = ipint & before
+	fin = ipint | after
+	return start, fin
+}
+
+func getMaskRange(mask string) (before uint, after uint) {
+	IntMask, _ := strconv.Atoi(mask)
+
+	before = uint(math.Pow(2, 32) - math.Pow(2, float64(32-IntMask)))
+	after = uint(math.Pow(2, float64(32-IntMask)) - 1)
+	return before, after
+}
+
+func ip2int(ipmask string) uint {
+	Ip := strings.Split(ipmask, "/")
+	s2ip := net.ParseIP(Ip[0]).To4()
+	return uint(s2ip[3]) | uint(s2ip[2])<<8 | uint(s2ip[1])<<16 | uint(s2ip[0])<<24
+}
+
+func Int2ip(ipint uint) string {
+	ip := make(net.IP, net.IPv4len)
+	ip[0] = byte(ipint >> 24)
+	ip[1] = byte(ipint >> 16)
+	ip[2] = byte(ipint >> 8)
+	ip[3] = byte(ipint)
+	return ip.String()
+}
