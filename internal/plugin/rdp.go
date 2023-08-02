c533ac38efbf29b26e59ea525461846ab2ba35e0
M09Ic <m09ician@gmail.com>
2023-08-02 15:55:18 +0800

    add rdp plugin

--- internal/plugin/rdp.go
+++ internal/plugin/rdp.go
@@ -1,58 +1,59 @@
 package plugin
 
-//import (
-//	"github.com/chainreactors/grdp"
-//	"github.com/chainreactors/grdp/glog"
-//	"github.com/chainreactors/zombie/pkg/utils"
-//	"strings"
-//)
-//
-//type RdpService struct {
-//	*utils.Task
-//	Input string
-//}
-//
-//func (s *RdpService) Query() bool {
-//	return false
-//}
-//
-//func (s *RdpService) GetInfo() bool {
-//	return false
-//}
-//
-//func (s *RdpService) Connect() error {
-//	err := RdpConnect(s.Task)
-//	if err != nil {
-//		return err
-//	}
-//	return nil
-//
-//}
-//
-//func (s *RdpService) Close() error {
-//	return nil
-//}
-//
-//func (s *RdpService) SetQuery(query string) {
-//	s.Input = query
-//}
-//
-//func (s *RdpService) Output(res interface{}) {
-//
-//}
-//
-//func RdpConnect(task *utils.Task) error {
-//	var username, domain string
-//	if strings.Contains(task.Username, "/") {
-//		username = strings.Split(task.Username, "/")[1]
-//		domain = strings.Split(task.Username, "/")[0]
-//	}
-//
-//	//SSL协议登录测试
-//	client := grdp.NewClient(task.Address(), glog.NONE)
-//	err := client.LoginForSSL(domain, username, task.Password)
-//	if err == nil {
-//		return nil
-//	}
-//	return nil
-//}
+import (
+	"fmt"
+	"github.com/chainreactors/zombie/pkg"
+	rdpclient "github.com/tomatome/grdp/client"
+	"sync"
+)
+
+type RdpService struct {
+	*pkg.Task
+}
+
+func (s *RdpService) Query() bool {
+	return false
+}
+
+func (s *RdpService) GetInfo() bool {
+	return false
+}
+
+func (s *RdpService) Connect() error {
+	err := RdpConnect(s.Task)
+	if err != nil {
+		return err
+	}
+	return nil
+}
+
+func (s *RdpService) Close() error {
+	return nil
+}
+
+func (s *RdpService) SetQuery(query string) {
+	//s.Input = query
+}
+
+func (s *RdpService) Output(res interface{}) {
+
+}
+
+func RdpConnect(task *pkg.Task) error {
+	client := rdpclient.NewClient(task.Address(), task.Username, task.Password, rdpclient.TC_RDP, nil)
+	if client == nil {
+		return fmt.Errorf("init error")
+	}
+	err := client.Login()
+	if err != nil {
+		return err
+	}
+	var wg sync.WaitGroup
+	wg.Add(1)
+	client.OnReady(func() {
+		fmt.Println("ready")
+		wg.Done()
+	})
+	wg.Wait()
+	return nil
+}
