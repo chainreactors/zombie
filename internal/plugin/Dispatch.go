1d452c66ea3438cc5718af9ba543e8333c1c56c2
Macchiatosheep <macchiatosheep@gmail.com>
2023-10-25 18:41:35 +0800

    pop3

--- internal/plugin/Dispatch.go
+++ internal/plugin/Dispatch.go
@@ -115,6 +115,10 @@ func Dispatch(task *pkg.Task) Plugin {
 		return &TelnetService{
 			Task: task,
 		}
+	case pkg.POP3Service:
+		return &Pop3Service{
+			Task: task,
+		}
 	default:
 		return nil
 	}
