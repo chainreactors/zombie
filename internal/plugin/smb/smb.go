f788b7e42648f5b816e4b3cfdb94bc27c8a05687
Macchiatosheep <macchiatosheep@gmail.com>
2023-11-26 23:19:13 +0800

    add UnAuthFunction

--- internal/plugin/smb/smb.go
+++ internal/plugin/smb/smb.go
@@ -18,7 +18,53 @@ type SmbPlugin struct {
 }
 
 func (s *SmbPlugin) Unauth() (bool, error) {
-	return false, nil
+	var user, domain string
+
+	if strings.Contains(s.Username, "/") {
+		user = strings.Split(s.Username, "/")[1]
+		domain = strings.Split(s.Username, "/")[0]
+	} else {
+		user = ""
+	}
+
+	c, err := net.DialTimeout("tcp", s.Address(), time.Duration(s.Timeout)*time.Second)
+	if err != nil {
+		return false, err
+	}
+
+	d := &smb2.Dialer{}
+	if strings.HasPrefix(s.Password, "hash:") {
+		hash := s.Password[5:]
+		buf := make([]byte, len(hash)/2)
+		hex.Decode(buf, []byte(hash))
+		d.Initiator = &smb2.NTLMInitiator{
+			User:   user,
+			Domain: domain,
+			Hash:   buf,
+		}
+	} else {
+		d.Initiator = &smb2.NTLMInitiator{
+			User:   user,
+			Domain: domain,
+			//Hash: buf,
+			Password: "",
+		}
+	}
+
+	_ = c.SetDeadline(time.Now().Add(time.Duration(s.Timeout) * time.Second))
+
+	conn, _, err := d.Dial(c)
+	if err != nil {
+		return false, err
+	}
+	// todo anon
+	_, err = conn.ListSharenames()
+	if err != nil {
+		return false, err
+	}
+	s.conn = conn
+
+	return true, nil
 }
 
 //func (s *SmbService) Query() bool {
