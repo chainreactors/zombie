c82b4419bee82c09c8500016e8f761b7b43753b3
M09Ic <m09ician@gmail.com>
2024-01-22 18:33:17 +0800

    fix output panic fix add every user task locked bug fix random seed not init bug

--- zombie.go
+++ zombie.go
@@ -1,7 +1,15 @@
 //go:generate go run templates/templates_gen.go -t templates -o pkg/templates.go -need zombie
 package main
 
-import "github.com/chainreactors/zombie/cmd"
+import (
+	"github.com/chainreactors/zombie/cmd"
+	"math/rand"
+	"time"
+)
+
+func init() {
+	rand.Seed(time.Now().Unix())
+}
 
 func main() {
 	cmd.Zombie()
