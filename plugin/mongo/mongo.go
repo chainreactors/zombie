949d3d9d39855f2b9bdd61f8c8253b506c0dabe0
M09Ic <m09ician@gmail.com>
2024-11-09 17:49:38 +0800

    remove mongo unauth, https://github.com/chainreactors/zombie/issues/30

--- plugin/mongo/mongo.go
+++ plugin/mongo/mongo.go
@@ -15,28 +15,29 @@ type MongoPlugin struct {
 }
 
 func (s *MongoPlugin) Unauth() (bool, error) {
-	var err error
-	var url string
-
-	if s.Password == "" {
-		url = fmt.Sprintf("mongodb://%v:%v", s.IP, s.Port)
-	} else {
-		url = fmt.Sprintf("mongodb://%v:%v@%v:%v", "mongodbuser", s.Password, s.IP, s.Port)
-	}
-	clientOptions := options.Client().ApplyURI(url).SetConnectTimeout(time.Duration(s.Timeout) * time.Second)
-
-	// 连接到MongoDB
-	client, err := mongo.Connect(s.Context, clientOptions)
-	if err != nil {
-		return false, err
-	}
-	s.conn = client
-	err = s.conn.Ping(s.Context, nil)
-	if err != nil {
-		return false, err
-	}
-
-	return true, nil
+	//var err error
+	//var url string
+	//
+	//if s.Password == "" {
+	//	url = fmt.Sprintf("mongodb://%v:%v", s.IP, s.Port)
+	//} else {
+	//	url = fmt.Sprintf("mongodb://%v:%v@%v:%v", "mongodbuser", s.Password, s.IP, s.Port)
+	//}
+	//clientOptions := options.Client().ApplyURI(url).SetConnectTimeout(time.Duration(s.Timeout) * time.Second)
+	//
+	//// 连接到MongoDB
+	//client, err := mongo.Connect(s.Context, clientOptions)
+	//if err != nil {
+	//	return false, err
+	//}
+	//s.conn = client
+	//err = s.conn.Ping(s.Context, nil)
+	//if err != nil {
+	//	return false, err
+	//}
+	//
+	//return true, nil
+	return false, pkg.NotImplUnauthorized
 }
 
 func (s *MongoPlugin) Name() string {
