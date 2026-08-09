fc4e6fc3220da131a0795b64c6103d3480b76ccc
M09Ic <m09ician@gmail.com>
2026-08-09 16:03:59 +0800

    fix: include zombie audit template

--- pkg/templates.go
+++ pkg/templates.go
@@ -9,6 +9,8 @@ import (
 	"github.com/chainreactors/utils/encode"
 )
 
+var RandomDir = "/g8kZMwp4oeKsL2in"
+
 //go:embed data/zombie_default.bin
 var zombieDefaultData []byte
 
@@ -39,26 +41,27 @@ var socketData []byte
 //go:embed data/http.bin
 var httpData []byte
 
+
 func loadEmbeddedConfig(typ string) []byte {
 	if typ == "zombie_default" {
 		return encode.MustDeflateDeCompress(zombieDefaultData)
-	} else if typ == "zombie_common" {
+	}else if typ == "zombie_common" {
 		return encode.MustDeflateDeCompress(zombieCommonData)
-	} else if typ == "zombie_rule" {
+	}else if typ == "zombie_rule" {
 		return encode.MustDeflateDeCompress(zombieRuleData)
-	} else if typ == "zombie_template" {
+	}else if typ == "zombie_template" {
 		return encode.MustDeflateDeCompress(zombieTemplateData)
-	} else if typ == "zombie_service" {
+	}else if typ == "zombie_service" {
 		return encode.MustDeflateDeCompress(zombieServiceData)
-	} else if typ == "zombie_loot" {
+	}else if typ == "zombie_loot" {
 		return encode.MustDeflateDeCompress(zombieLootData)
-	} else if typ == "zombie_audit" {
+	}else if typ == "zombie_audit" {
 		return encode.MustDeflateDeCompress(zombieAuditData)
-	} else if typ == "port" {
+	}else if typ == "port" {
 		return encode.MustDeflateDeCompress(portData)
-	} else if typ == "socket" {
+	}else if typ == "socket" {
 		return encode.MustDeflateDeCompress(socketData)
-	} else if typ == "http" {
+	}else if typ == "http" {
 		return encode.MustDeflateDeCompress(httpData)
 	}
 	return nil
